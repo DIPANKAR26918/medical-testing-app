ce25d68a7ea6cc66eb22b6ef4b0f2019d5493afa
Dipangkar <dipankarsarkar001cobz@gmail.com>
2026-06-20 15:30:15 +0530

    added logic and images to banners.dart

---
+++ lib/widgets/banners.dart
@@ -0,0 +1,79 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+class HomeBanner extends StatefulWidget {
+  const HomeBanner({super.key});
+
+  @override
+  State<HomeBanner> createState() => _HomeBannerState();
+}
+
+class _HomeBannerState extends State<HomeBanner> {
+  int _currentIndex = 0;
+
+  final List<String> banners = [
+    'assets/images/fullbody_checkup_banner1.jpeg',
+    'assets/images/diabetes_screening_banner.jpeg',
+    'assets/images/quick_reports_banner.jpeg',
+    'assets/images/join_healthplus_banner.jpeg',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CarouselSlider(
+          options: CarouselOptions(
+            height: 180,
+            autoPlay: true,
+            autoPlayInterval: const Duration(seconds: 3),
+            autoPlayAnimationDuration: const Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enableInfiniteScroll: true,
+            enlargeCenterPage: true,
+            viewportFraction: 0.92,
+
+            onPageChanged: (index, reason) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+          ),
+          items: banners.map((imagePath) {
+            return Container(
+              width: double.infinity,
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+              ),
+              clipBehavior: Clip.antiAlias,
+              child: Image.asset(imagePath, fit: BoxFit.cover),
+            );
+          }).toList(),
+        ),
+
+        const SizedBox(height: 12),
+
+        // Dots Indicator
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List.generate(
+            banners.length,
+            (index) => AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              margin: const EdgeInsets.symmetric(horizontal: 4),
+              width: _currentIndex == index ? 20 : 8,
+              height: 8,
+              decoration: BoxDecoration(
+                color: _currentIndex == index
+                    ? const Color(0xff0E8C93)
+                    : Colors.grey.shade300,
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
