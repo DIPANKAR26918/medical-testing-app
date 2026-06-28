ce2638b1eca94fd5550fe5aeb6558ddfdf3cd267
Dipangkar <dipankarsarkar001cobz@gmail.com>
2026-06-28 19:08:18 +0530

    added the new stuffs(eg. bookings_screen.dart,home_dashboard_screen.dart etc)

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -2,7 +2,14 @@
 export 'welcome_screen.dart';
 export 'authentication_screen.dart';
 export 'otp_screen.dart';
+export 'complete_profile_screen.dart';
 export 'home_screen.dart';
+export 'bookings_screen.dart';
+export 'reports_screen.dart';
+export 'profile_screen.dart';
+export 'home_dashboard_screen.dart';
+export 'all_categories_page.dart';
+export 'search_screen.dart';
 export 'upload_prescription_screen.dart';
 export 'order_details_screen.dart';
 export 'test_status_screen.dart';
